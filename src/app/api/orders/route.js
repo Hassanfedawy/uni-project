9a52269a2bc2702d84d263bee393fe2bfcc6f927
Alhassan <alhassanfedawy620@gmail.com>
2024-12-30 20:31:25 +0200

    deploy issue

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../../lib/auth';
+import { authOptions } from '../../../../lib/auth';
 import { ObjectId } from 'mongodb';
 
 const prisma = new PrismaClient();
